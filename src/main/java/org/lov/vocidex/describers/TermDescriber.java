028264d3d20df1b59c9613ce72be9ffbba1cf406
Pierre-Yves Vandenbussche <py.vandenbussche@gmail.com>
2015-01-14 01:23:13 +0000

    Fail attempt to use suggest completion for terms

--- src/main/java/org/lov/vocidex/describers/TermDescriber.java
+++ src/main/java/org/lov/vocidex/describers/TermDescriber.java
@@ -1,8 +1,12 @@
 package org.lov.vocidex.describers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.codehaus.jackson.node.ObjectNode;
 import org.lov.SPARQLRunner;
 
+import com.hp.hpl.jena.rdf.model.Literal;
 import com.hp.hpl.jena.rdf.model.Resource;
 
 public abstract class TermDescriber extends SPARQLDescriber {
@@ -24,6 +28,13 @@ public abstract class TermDescriber extends SPARQLDescriber {
 	public String getLocalName(Resource term) {
 		return term.getLocalName();
 	}
+	
+	private List<Literal> getRDFSLabels(Resource term) {
+		return getSource().getLiterals("term-labels.sparql","label", "term", term);
+	}
+	private String getLabelFromSource(Resource term) {
+		return getSource().getLangString("term-label.sparql", term, "label");
+	}
 
 //	public String getComment(Resource term) {
 //		return getSource().getLangString("term-comment.sparql", term, "comment");
@@ -38,6 +49,15 @@ public abstract class TermDescriber extends SPARQLDescriber {
 		};
 		putString(descriptionRoot, "localName", getLocalName(term));
 		
+//		List<Literal> labels = getRDFSLabels(term);
+//		List<String> labelsString = new ArrayList<String>();
+//		if(labels!=null){
+//			for (Literal label : labels) {
+//				labelsString.add(label.getLexicalForm());
+//			}
+//			if(labelsString.size()>0)putArrayString(descriptionRoot, "labelsWithoutLang", labelsString);
+//		}
+		//putString(descriptionRoot, "labelsWithoutLang", getLabelFromSource(term));
 		if(tag!=null)putArrayString(descriptionRoot, "tags", tag);
 		// Adds "label" key
 		strLiteralDescriber.describe(term, descriptionRoot);
